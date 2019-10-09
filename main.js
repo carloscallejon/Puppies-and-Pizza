e1ecb4bd5e01f850e8aa7c2ff861d52c1e3b6e6e
Carlos Callejon <ccallejonh@gmail.com>
2019-10-08 21:01:38 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var chance, itemToRender;
+
+
+function showPizzaPuppy() {
+  chance = Math.random();
+  if(chance>0.5){
+    // show puppy :
+    itemToRender = "puppies";
+
+    renderItem(itemToRender);
+  } else{
+    // show pizza : 
+    itemToRender = "pizza";
+
+    renderItem(itemToRender);
+  }
+}
+
+
+function renderItem(str){
+  fetch(`https://source.unsplash.com/1600x900/?${str}`).then((response)=> {
+    document.getElementById('imageContainer').style.visibility = "visible";
+    console.log("here");
+    document.getElementById('imageToShow').src = `${response.url}`;
+  })
+}
